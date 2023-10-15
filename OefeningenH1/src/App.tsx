48212cb79820734be8c13943e2ee5c94916f01ca
Erika <r0231723@student.thomasmore.be>
2023-10-15 15:23:11 +0200

    Oefening 1 klaar.

--- OefeningenH1/src/App.tsx
+++ OefeningenH1/src/App.tsx
@@ -1,11 +1,14 @@
 import Exercise from './exercise/exercise.tsx'
+import ExerciseOne from './01-exerciseOne/exerciseOne.tsx'
 
 function App() {
 
 
     return (
         <>
-            <Exercise title={'Exercise 1: Multiplication Table'}/>
+            <Exercise title={'Exercise 1: Multiplication Table'}>
+                <ExerciseOne/>
+            </Exercise>
             <Exercise title={'Exercise 2: Rater'} background={'#77EEEE'}/>
             <Exercise title={'Exercise 3: Progressbar'}/>
             <Exercise title={'Exercise 4: Number grid'} background={'#77EEEE'}/>
