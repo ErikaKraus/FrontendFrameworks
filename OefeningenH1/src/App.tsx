ece2fd1b39f8d4409b8f1bd1f89ed192c84a2f13
Erika <r0231723@student.thomasmore.be>
2023-10-15 15:03:45 +0200

    Prepwork finished.

--- OefeningenH1/src/App.tsx
+++ OefeningenH1/src/App.tsx
@@ -5,7 +5,11 @@ function App() {
 
     return (
         <>
-            <Exercise title={'Title'}/>
+            <Exercise title={'Exercise 1: Multiplication Table'}/>
+            <Exercise title={'Exercise 2: Rater'} background={'#77EEEE'}/>
+            <Exercise title={'Exercise 3: Progressbar'}/>
+            <Exercise title={'Exercise 4: Number grid'} background={'#77EEEE'}/>
+            <Exercise title={'Exercise 5: Comment card'}/>
         </>
     )
 }
--- OefeningenH1/src/exercise/exerciseTitle.tsx
+++ OefeningenH1/src/exercise/exerciseTitle.tsx
@@ -1,4 +1,15 @@
 import {FunctionComponent} from 'react'
+import styled from 'styled-components'
+
+const TitleContainer = styled.div`
+  font-family: "Lucida Sans", Monaco, monospace;
+  font-size: 3rem;
+  letter-spacing: 3px;
+  color: #3A5D9B;
+  font-weight: 300;
+  font-style: oblique;
+  line-height: 1.2;`
+
 
 interface ExerciseTitleProps {
 title: string
@@ -7,7 +18,10 @@ title: string
 const ExerciseTitle: FunctionComponent<ExerciseTitleProps> = ({title}) => {
     return (
         <>
-            {title}
+            <TitleContainer>
+                {title}
+            </TitleContainer>
+
         </>
     )
 }
